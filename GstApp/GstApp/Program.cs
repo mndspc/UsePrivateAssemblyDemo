bbf4a8b0cddf077e2a0081e851e12782131a0ce8
Shoaib.Ghachi <shoaib.ghachi@hotmail.com>
2021-12-09 18:17:59 +0530

    Pushing new project
    contains how to use private assembly

---
+++ GstApp/GstApp/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using GstUtilities;
+
+namespace GstApp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Gst gst = new Gst(10000, 10);
+            Console.WriteLine($"GST Amount is:{gst.CalculateGstAmt()}");
+            Console.WriteLine($"Total Product Amount is:{gst.CalculateTotalProAmt()}");
+            Console.ReadLine();
+        }
+    }
+}
